ac758a761230a967901043df3fe562d6dbe7e86b
SevenSeven-7 <senimansetia2@gmail.com>
2026-05-28 21:39:34 +0700

    perombakan arsitektur audio antarmuka menjadi sistem kolam suara (Mengganti pemutar suara tunggal dengan sekumpulan agen pemutar untuk mencegah jank dan tabrakan frekuensi saat efek diketuk dengan cepat)

--- lib/inti/utils/audio_helper.dart
+++ lib/inti/utils/audio_helper.dart
@@ -2,7 +2,13 @@ import 'package:audioplayers/audioplayers.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AudioHelper {
-  static final AudioPlayer _clickPlayer = AudioPlayer();
+  // Gunakan sistem "Pool" (kolam) dengan 5 AudioPlayer 
+  // agar suara klik bisa bertumpuk (overlapping) tanpa patah-patah/stutter saat ditekan cepat
+  static final List<AudioPlayer> _clickPlayers = List.generate(
+    3, 
+    (_) => AudioPlayer(),
+  );
+  static int _currentPlayerIndex = 0;
   static bool? _isSoundEnabled;
 
   static Future<void> playClick() async {
@@ -12,7 +18,11 @@ class AudioHelper {
         _isSoundEnabled = prefs.getBool('pengaturan_suara') ?? true;
       }
       if (_isSoundEnabled!) {
-        await _clickPlayer.play(AssetSource('sounds/suara-click.mp3'));
+        final player = _clickPlayers[_currentPlayerIndex];
+        _currentPlayerIndex = (_currentPlayerIndex + 1) % _clickPlayers.length;
+        
+        await player.stop();
+        await player.play(AssetSource('sounds/suara-click.mp3'));
       }
     } catch (e) {
       // Abaikan jika error
