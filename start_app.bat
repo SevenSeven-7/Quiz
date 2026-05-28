5fa7a8c72d330eddaaca96b5339562e26553c4dc
SevenSeven-7 <senimansetia2@gmail.com>
2026-05-28 12:39:19 +0700

    Pembersihan Skrip Usang

--- start_app.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo ================================================
-echo Starting Quiz Flutter App
-echo ================================================
-cd Quiz_App
-flutter run -d chrome
-pause
--- start_server.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo ================================================
-echo Starting Quiz Dataset API Server
-echo ================================================
-cd Quiz_Dataset
-python api_server.py
-pause
