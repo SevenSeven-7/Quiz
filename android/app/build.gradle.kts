226ac33e50a80855cffa86838dba1f0c14077ef7
SevenSeven-7 <senimansetia2@gmail.com>
2026-06-24 10:59:22 +0700

    Pembaruan Konfigurasi Rilis, Package Name, dan Perbaikan Bug iOS
    Rincian pembaruan:
    
    1. Mengubah package name (Bundle ID) dari com.example.quiz menjadi com.syamsularifin.quiz secara menyeluruh.
    
    2. Menambahkan konfigurasi keystore rilis di build.gradle.kts Android.
    
    3. Memperbarui pubspec.yaml: penambahan package rename & logger, serta menaikkan versi menjadi 1.0.0+2.
    
    4. Menghapus permission USE_EXACT_ALARM di AndroidManifest.xml.
    
    5. Menambahkan aset gambar baru (banner_1024x500.png).
    
    6. Memperbaiki konflik Bundle ID pada target iOS RunnerTests agar tidak bentrok dengan target utama.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -6,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.quiz"
+    namespace = "com.syamsularifin.quiz"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -20,9 +23,24 @@ android {
         jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
+    val keystorePropertiesFile = rootProject.file("key.properties")
+    val keystoreProperties = Properties()
+    if (keystorePropertiesFile.exists()) {
+        keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+    }
+
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties.getProperty("keyAlias")
+            keyPassword = keystoreProperties.getProperty("keyPassword")
+            storeFile = file(keystoreProperties.getProperty("storeFile"))
+            storePassword = keystoreProperties.getProperty("storePassword")
+        }
+    }
+
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.quiz"
+        applicationId = "com.syamsularifin.quiz"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -35,7 +53,7 @@ android {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
--- android/app/src/main/kotlin/com/syamsularifin/quiz/MainActivity.kt
+++ android/app/src/main/kotlin/com/syamsularifin/quiz/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.quiz
+package com.syamsularifin.quiz
 
 import io.flutter.embedding.android.FlutterActivity
 
